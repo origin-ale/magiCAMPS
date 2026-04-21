52d24687f377a69018bd6000a05fdf9ed5fa697d
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-04-21 10:01:15 +0200

    Generalize to arbitrary Pauli/phase lists

---
+++ evolve_gencircuit.jl
@@ -0,0 +1,48 @@
+using CliffordMPS
+using QuantumClifford
+using ITensors, ITensorMPS
+using ProgressMeter
+
+function evolve_gencircuit(ψ::CAMPS,
+                           t::Int,
+                           paulistrings,
+                           phases;
+                           showprogress = false)
+  k = 0
+  progressbar = Progress(t; desc = "Evolving…", enabled = showprogress)
+  for s in 1:t
+    applyQOp!(ψ, QOp(:randCliffCircuit))
+
+    P = paulistrings[s]
+    ϕ = phases[s]
+    N = length(ψ)
+    I = PauliOperator(0x0, fill(false,N), fill(false,N))
+    C = inv(ψ.Cdag)
+
+    nature = paulinature(k, C, P)
+    if nature == :disentanglable
+      ψ.Cdag *= inv(disentangler(k, C, P))
+      addmagicstate!(ψ, k, ϕ)
+      k += 1
+    elseif nature == :logical
+      R = PauliSum([cos(ϕ), sin(ϕ)], Stabilizer([I,P]))
+      applyGate!(ψ, R)
+    elseif nature == :trivial
+      next!(progressbar)
+      continue
+    end
+    next!(progressbar)
+  end
+  return ψ, k
+end
+
+"Turn the (k+1)th qubit from |0⟩ to the Liu and Clark (2025) magic state"
+function addmagicstate!(ψ::CAMPS, k::Integer, phase::Real)
+  magifier_os = OpSum()
+  magifier_os += cos(phase), "Id", k+1
+  magifier_os += -im * sin(phase), "X", k+1
+  sites = siteinds(ψ.mps)
+  magifier = MPO(magifier_os, sites)
+  ψ.mps = ITensors.apply(magifier, ψ.mps)
+  return nothing
+end
---
+++ gencircuit.jl
@@ -0,0 +1,28 @@
+include("algorithm.jl")
+include("evolve_gencircuit.jl")
+
+using CliffordMPS
+using QuantumClifford
+using ITensors, ITensorMPS
+using ProgressMeter
+
+N = length(ARGS) >= 1 ? parse(Int, ARGS[1]) : 10
+t = length(ARGS) >= 2 ? parse(Int, ARGS[2]) : 10
+xbits = fill(false, N)
+zbits = fill(false, N)
+zbits[1] = true
+Z₁ = PauliOperator(0x0, xbits, zbits)
+
+paulistrings = fill(Z₁, t)
+phases = fill(-π/8, t)
+
+ψ = CAMPS(N) # Initialize CAMPS
+ψ_evo, k = evolve_gencircuit(ψ, t, paulistrings, phases; showprogress = true)
+
+println("$(N-k) free qubit(s) left.")
+print("Final MPS:")
+println(ψ_evo.mps)
+avg_ee = maximum(eEntropys!(ψ_evo.mps))/N
+println("Avg entanglement entropy $(avg_ee)")
+avg_sre = sEntropy(ψ_evo.mps, N^2; α =2)/N
+println("Avg 2-SRE $(avg_sre)")
